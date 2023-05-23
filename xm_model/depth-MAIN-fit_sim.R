c40dbd4270843113227698dbf722c523cf3bb205
Jesse Windle <jesse@bayesfactor.net>
2023-05-23 10:56:19 -0500

    Changed to SESSION_ID in Main fit sim

--- xm_model/depth-MAIN-fit_sim.R
+++ xm_model/depth-MAIN-fit_sim.R
@@ -10,10 +10,10 @@ library("yaml")
 MODELS_TO_RUN = c(2, 5, 6)
 
 USE = 0.1
-ITER = 3000
-WARMUP = 1500
-CHAINS = 2
-CORES = 2
+ITER = 800
+WARMUP = 400
+CHAINS = 1
+CORES = 1
 
 
 config = read_yaml("depth-MAIN-fit_sim.yaml")
@@ -26,14 +26,15 @@ pars_of_interest[[5]] = c("mu_dm", "sig_dm", "shape_dm")
 pars_of_interest[[6]] = c("mu_dm", "sig_dm")
 
 
-# SESSION_UUID = "b7f0512c-f0d6-11ed-af6b-8c8590d39d9a"
-SESSION_UUID = "e0236604-f0fc-11ed-ab75-8c8590d39d9a"
+# SESSION_ID = "b7f0512c-f0d6-11ed-af6b-8c8590d39d9a"
+# SESSION_ID = "e0236604-f0fc-11ed-ab75-8c8590d39d9a"
+SESSION_ID = "TEST"
 
 for (model_num in MODELS_TO_RUN) {
   
   # Stan files
   stan_fit_file = sprintf("depth-m%02d-fit.stan", model_num)
-  load_file = file.path("..", "data", sprintf("%s-m%02d-sim.RData", SESSION_UUID, model_num))
+  load_file = file.path("..", "data", sprintf("%s-m%02d-sim.RData", SESSION_ID, model_num))
 
   # Load data
   if (!file.exists(load_file)) {
@@ -87,13 +88,13 @@ for (model_num in MODELS_TO_RUN) {
   p_hist = mcmc_recover_hist(samp_array, param) + ggtitle(sprintf("Posteriors M%02d", model_num))
   p_hist
 
-  p_hist_file = sprintf("%s-m%02d-p_hist.png", SESSION_UUID, model_num)
+  p_hist_file = sprintf("%s-m%02d-p_hist.png", SESSION_ID, model_num)
   ggsave(p_hist, file=file.path("..", "images", "xm_model", p_hist_file))
 
   p_pairs = mcmc_pairs(samp_array, grid_args = list(top=sprintf("Pairs M%02d", model_num)))
   p_pairs
 
-  p_pairs_file = sprintf("%s-m%02d-p_pairs.png", SESSION_UUID, model_num)
+  p_pairs_file = sprintf("%s-m%02d-p_pairs.png", SESSION_ID, model_num)
   ggsave(p_pairs, file=file.path("..", "images", "xm_model", p_pairs_file))
 
   # # Stan versions of plotting routines
