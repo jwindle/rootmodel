b93102f25eaea39fc5bc606c0f9fcd327bae29d4
Jesse Windle <jesse@bayesfactor.net>
2023-07-08 16:17:36 -0500

    Added model and changed config schema for sim_and_plot

--- xm_model/depth-MAIN-sim_and_plot.R
+++ xm_model/depth-MAIN-sim_and_plot.R
@@ -1,31 +1,33 @@
 # -*- ess-style: RStudio; -*-
 
-source("../shared/functions.R")
+source("shared/functions.R")
 
 library("uuid")
 library("yaml")
 
-# Config
-MODELS_TO_RUN = c(2, 5, 6)
-
-config = read_yaml("depth-MAIN-sim_and_plot.yaml")
-
+config = read_yaml("xm_model/depth-MAIN-sim_and_plot.yaml")
 param_config = config[["stan_data"]]
+
+MODELS_TO_RUN = config$models_to_run
+MODELS_TO_RUN
+
+
 
 # Generate UUID to use for filenames
 SESSION_ID = "TEST"
 # SESSION_ID = UUIDgenerate(use.time=TRUE)
 
 
-for (model_num in MODELS_TO_RUN) {
+for (model_name in MODELS_TO_RUN) {
 
   # File names
-  stan_sim_file = sprintf("depth-m%02d-sim.stan", model_num)
-  save_file = file.path("..", "data", sprintf("%s-m%02d-sim.RData", SESSION_ID, model_num))
+  stan_sim_file = file.path("xm_model", sprintf("depth-%s-sim.stan", model_name))
+  save_file = file.path("data", sprintf("%s-%s-sim.RData", SESSION_ID, model_name))
 
   # Param
-  fixed_param = make_base_data(param_config[[model_num]])
-  param_df = make_parameter_df(param_config[[model_num]])
+  fixed_param = make_base_data(param_config[[model_name]])
+  param_df = make_parameter_df(param_config[[model_name]])
+  param_df$mu_dx = fixed_param$r / (param_df$K + 1)
   
   # Compile
   model = stan_model(stan_sim_file, model_name=stan_sim_file, verbose=TRUE)
@@ -43,3 +45,18 @@ for (model_num in MODELS_TO_RUN) {
   save(depth_list, param_df, file=save_file)
   
 }
+
+
+
+## # Extract param names
+## p_depths = depths_df %>%
+##   filter(K == 3) %>%
+##   ggplot() +
+##   geom_histogram(aes(x=depth, y=..density.., fill=as.factor(mu_yp)), bins=10) +
+##   facet_grid(col=vars(mu_yp), row=vars(sig_yp)) +
+##   xlab("depth (cm)") +
+##   ggtitle(TeX("Distribution of depths")) +
+##   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+##   coord_flip()
+
+## p_depths
