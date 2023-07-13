bf5677642dd3992294d59b57a4e62f973e37f652
Jesse Windle <jesse@bayesfactor.net>
2023-07-13 13:01:07 -0500

    Added m23 --- GP version of m10

---
+++ xm_model/depth-m23-fit.R
@@ -0,0 +1,145 @@
+# -*- ess-style: RStudio -*- #
+# Jesse Windle
+
+library("ggplot2")
+library("gridExtra")
+
+library("rstan")
+
+source("shared/functions.R")
+
+# Load
+df_all = load_rt_data("../data/acc-1-v3.csv") %>%
+    filter(uptime > 0, electrode_pair > 1, days_since_start > 4, crop == "Corn")
+
+df_all$old_count = df_all$count
+df_all$count = 1 * (df_all$old_count > 0)
+
+time_grid = unique(df_all$days_since_start)
+df_all$time_group =  match(df_all$days_since_start, time_grid)
+
+
+# Simplify and plot data
+
+depths_df = df_all %>%
+    filter(
+        count > 0,
+        crop == "Corn"
+    )
+
+
+y_data = depths_df$depth_cm
+n_obs = length(y_data)
+time_group = depths_df$time_group
+n_times = max(time_group)
+
+
+# Model
+mdl_23 = stan_model("xm_model/depth-m23-fit.stan", model_name="depth-m23-fit", verbose=TRUE)
+
+# Data
+dat = list(
+  n_obs = n_obs,
+  n_times = n_times,
+  time_grid = 1:M,
+  time_group = time_group,
+  r = 8.0,
+  mu_dx = 4.,
+  K = 1,
+  y = y_data,
+  mu_y_prior_mean = 8.,
+  mu_y_prior_std = 4.,
+  sig_y_prior_mean = 4,
+  sig_y_prior_std = 4,
+  shape_dm_prior_mean = 0,
+  shape_dm_prior_std = 5
+)
+
+# Fit - had to up adapt_delta to 0.95 to git rid of divergent
+# transitions.
+out_23 = sampling(
+  mdl_23,
+  data=dat,
+  iter=1000,
+  warmup=500,
+  chains=1,
+  cores=1,
+  control = list(
+    adapt_delta = 0.99,
+    adapt_t0 = 10
+  )
+)
+
+# Summary and pairs plot
+static_pars = c("mu_y_gbl_mean", "sig_y_gbl_mean", "shape_dm_gbl_mean")
+
+proc_pars = c("mu_y", "sig_y", "shape_dm")
+
+samp_df = extract(out_23, static_pars) %>% as_tibble()
+
+summary(out_23, pars=static_pars)
+
+pairs(out_23, pars=static_pars)
+
+
+# Diagnositcs
+stan_diag(out_23, information="divergence")
+
+stan_diag(out_23, information="stepsize")
+
+stan_diag(out_23, information="sample")
+
+stan_mcse(out_23, pars=poi)
+
+
+# Proccesses
+f_gp = extract(out_23, "f_gp")[[1]]
+
+dim(f_gp)
+f_gp_mean = apply(f_gp, c(2, 3), mean)
+
+par(mfrow=c(1,3))
+plot(f_gp_mean[1,], main="mu_y deviations", type="l")
+plot(f_gp_mean[2,], main="sig_y deviations", type="l")
+plot(f_gp_mean[3,], main="shape_dm deviations", type="l")
+
+
+# Extract posterior samples of interest
+time_var_samp = extract(out_23, c("mu_y", "sig_y", "shape_dm"))
+
+time_var_q_list = lapply(
+  time_var_samp,
+  function(x, tg) {
+    temp_df = as.data.frame(t(apply(x, 2, quantile, c(0.25, 0.5, 0.75))))
+    temp_df$time_grid = tg
+    temp_df
+  },
+  time_grid
+)
+
+time_var_df = bind_rows(time_var_q_list, .id = "var")
+head(time_var_df)
+
+
+# Plot mean process
+time_var_df %>% filter(var == "mu_y") %>%
+  ggplot(aes(time_grid, `50%`)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=`25%`, ymax=`75%`), alpha=0.25)
+
+
+# Plot sd process
+time_var_df %>% filter(var == "sig_y") %>%
+  ggplot(aes(time_grid, `50%`)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=`25%`, ymax=`75%`), alpha=0.25)
+
+
+# Plot shape process
+time_var_df %>% filter(var == "shape_dm") %>%
+  ggplot(aes(time_grid, `50%`)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=`25%`, ymax=`75%`), alpha=0.25)
+
+
+
---
+++ xm_model/depth-m23-fit.stan
@@ -0,0 +1,91 @@
+/* -*- c-basic-offset: 4; -*- */
+#include "common.stan"
+data {
+	int<lower=1> n_obs;
+	int<lower=1> n_times;
+	real time_grid[n_times];
+	int<lower=1, upper=n_times> time_group[n_obs];
+	real<lower=0.> r;
+	real<lower=0.> mu_dx;
+	int<lower=1> K;
+	real y[n_obs];
+	real mu_y_prior_mean;
+	real mu_y_prior_std;
+	real sig_y_prior_mean;
+	real sig_y_prior_std;
+	real shape_dm_prior_mean;
+	real shape_dm_prior_std;
+}
+transformed data {
+	vector[K+1] x_left = x_left_from_mu_dx(r, mu_dx, K);
+	real x_left_l1 = sum(x_left);
+	real x_left_l2 = l2_norm(x_left);
+	real mu_dm_prior_mean = mu_y_prior_mean / x_left_l1;
+	real mu_dm_prior_std = mu_y_prior_std / x_left_l1;
+	real sig_dm_prior_mean = sig_y_prior_mean / x_left_l2;
+	real sig_dm_prior_std = sig_y_prior_std / x_left_l2;
+	real sqrt_2_over_pi = sqrt(2. / pi());
+	real tiny = 1e-9; // Try to ensure positive definite
+	real sqrt_tiny = sqrt(tiny);
+}
+parameters {
+	/* Gaussian process */
+	real<lower=0.> gp_ell[3];
+	real<lower=0.> gp_sig[3];
+	vector[n_times] eta[3];
+	/* Global parameters values */
+	real mu_y_gbl_mean;
+	real<lower=0.> sig_y_gbl_mean;
+	real shape_dm_gbl_mean;
+	/* Changes in slope */
+	vector[K+1] dm[n_obs];
+}
+transformed parameters {
+	/* Gaussian process */
+	vector[n_times] f_gp[3];
+	for (i in 1:3) {
+		matrix[n_times, n_times] L;
+		matrix[n_times, n_times] C = gp_exp_quad_cov(time_grid, gp_sig[i], gp_ell[i]);
+		// diagonal elements - this may not be necessary since I
+		// control the design, i.e. `time_grid`.
+		for (n in 1:n_times) {
+			C[n, n] = C[n, n] + tiny;
+		}
+		L = cholesky_decompose(C);
+		f_gp[i] = L * eta[i];
+	}
+	/* Transformation to location, scale, shape */
+	vector[n_times] mu_dm = (f_gp[1] + mu_y_gbl_mean) / x_left_l1;
+	vector<lower=0.>[n_times] sig_dm = (exp(f_gp[2]-0.5*square(gp_sig[2])) * sig_y_gbl_mean) / x_left_l2;
+	vector[n_times] shape_dm = f_gp[3] + shape_dm_gbl_mean;
+	vector[n_times] delta_dm = shape_dm ./ sqrt(1. + square(shape_dm));
+	vector[n_times] b_delta_dm = sqrt_2_over_pi * delta_dm;
+	vector<lower=0.>[n_times] omega_dm = sig_dm ./ sqrt(1. - square(b_delta_dm));
+	vector[n_times] xi_dm = mu_dm - b_delta_dm .* omega_dm;
+	/* Transformation to depths */
+	real z[n_obs];
+	for (i in 1:n_obs) {
+		z[i] = dot_product(dm[i], x_left);
+	}
+}
+model {
+	gp_ell ~ inv_gamma(5, 5);
+	gp_sig ~ std_normal();
+	mu_y_gbl_mean ~ normal(mu_y_prior_mean, mu_y_prior_std);
+	sig_y_gbl_mean ~ normal(sig_y_prior_mean, sig_y_prior_std) T[0,];
+	shape_dm_gbl_mean ~ normal(shape_dm_prior_mean, shape_dm_prior_std);
+	for (i in 1:3) {
+		eta[i] ~ std_normal();
+	}
+	for (i in 1:n_obs) {
+		int t = time_group[i];
+		dm[i] ~ skew_normal(xi_dm[t], omega_dm[t], shape_dm[t]);
+		y[i] ~ normal(z[i], 0.01);
+	}
+}
+generated quantities {
+	real mu_dm_gbl_mean = mu_y_gbl_mean / x_left_l1;
+	real<lower=0.> sig_dm_gbl_mean = sig_y_gbl_mean / x_left_l2;
+	vector[n_times] mu_y = mu_dm * x_left_l1;
+	vector<lower=0.>[n_times] sig_y = sig_dm * x_left_l2;
+}
